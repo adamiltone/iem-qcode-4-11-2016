72d920c31092e711caee3af4a1c4f7b9b908d9b6
antdesc <adescamps@amiltone.com>
2016-10-28 17:00:22 +0200

    [FEAT] question 1.3

--- app/src/main/java/com/amiltone/td_iem_411/MainActivity.java
+++ app/src/main/java/com/amiltone/td_iem_411/MainActivity.java
@@ -2,12 +2,39 @@ package com.amiltone.td_iem_411;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.activity_main_tv_content) TextView tvContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.activity_main_button_associate)
+    public void actionAssociate(Button button) {
+        updateContent("Associate clicked");
+    }
+
+    @OnClick(R.id.activity_main_button_detail)
+    public void actionDetail(Button button) {
+        updateContent("Detail clicked");
+    }
+
+    @OnClick(R.id.activity_main_button_retrieve)
+    public void actionRetrieve(Button button) {
+        updateContent("Retrieve clicked");
+    }
+
+    private void updateContent(String value) {
+        tvContent.setText(value);
     }
 }
